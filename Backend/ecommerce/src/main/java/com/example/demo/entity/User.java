8623f327d0de5af699645f8f9ab6b5edba356924
Pramodh Reddy <pramodhreddyn@gmail.com>
2025-03-28 20:33:23 +0530

    Initialized Spring Boot backend

---
+++ Backend/ecommerce/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,28 @@
+package com.example.demo.entity;
+
+
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role; // USER or ADMIN
+}
